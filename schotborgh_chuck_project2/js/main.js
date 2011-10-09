49018fddd03f4d99ea451f398fbde27bf1eb7f32
Chuck Schotborgh <chuckschotborgh@fullsail.edu>
2011-10-09 10:51:08 -0700

    Yep!

--- schotborgh_chuck_project2/js/main.js
+++ schotborgh_chuck_project2/js/main.js
@@ -87,32 +87,32 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 	
 	function getData(){
-	toggleControls("on");
-	if(localStorage.length === 0){
-		alert("There is no data in Local Storage.");
-	}
-	
-	//Write Data from Local Storage to the Browser.
-	var makeDiv = document.createElement('div');
-	makeDiv.setAttribute("id", "items");
-	var makeList = document.createElement('ul');
-	makeDiv.appendChild(makeList);
-	document.body.appendChild(makeDiv);
-	$('items').style.display = "block";
-	for(var i=0, len=localStorage.length; i<len; i++){
-		var makeli = document.createElement('li');
-		makelist.appendChild(makeli);
-		var key = localStorage.key(i);
-		var value = localStorage.getItem(key);
-		//convert the string from local storage value back to an object by using JSON.parse
-		var obj = JSON.parse(value);
-		var makeSubList = document.createElement('ul');
-		makeli.appendChild(makeSubList);
-		for(var n in obj){
-			var makeSubli = document.createElement('li');
-			makeSubList.appendChild(makeSubli);
-			var optSubText = obj[n][0]+" "+obj[n][1];
-			makeSubli.innerHTML = optSubText;
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no data in Local Storage.");
+		}
+		
+		//Write Data from Local Storage to the Browser.
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len=localStorage.length; i<len; i++){
+			var makeli = document.createElement('li');
+			makelist.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//convert the string from local storage value back to an object by using JSON.parse
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeli.appendChild(makeSubList);
+			for(var n in obj){
+				var makeSubli = document.createElement('li');
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
 		}
 	}
 }
